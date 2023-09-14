9b5a30a308f33060ee0d75228e76b05fd7488e1a
Rohit Sen <90069220+rohitsen49@users.noreply.github.com>
2023-09-13 23:02:41 -0700

    Add files via upload

---
+++ RESTServiceProjectClient/Program.cs
@@ -0,0 +1,10 @@
+﻿var client = new HttpClient();
+client.BaseAddress = new Uri("http://localhost:#####/api/");
+// client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+var result = await client.GetAsync("users");
+
+var json = await result.Content.ReadAsStringAsync();
+
+Console.WriteLine(json);
+Console.ReadLine();
